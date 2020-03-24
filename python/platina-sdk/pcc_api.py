77b9b36d260a1cca026da6bed98a3366b150a89f
Mario <mario.suman@gmail.com>
2020-03-24 05:52:15 +0000

    add provisions

--- python/platina-sdk/pcc_api.py
+++ python/platina-sdk/pcc_api.py
@@ -32,6 +32,8 @@ PCC_MAAS = PCCSERVER + "/maas"
 PCC_NOTIFICATIONS = PCCSERVER + "/notifications"
 PCC_PORTUS = PCCSERVER + "/portus"
 PCC_PROFILE = PCCSERVER + "/profile"
+PCC_PROVISIONS = PCCSERVER + "/provisions"
+
 
 ## Login
 def login(url:str, username:str, password:str)->dict:
@@ -2343,7 +2345,260 @@ def get_authentication_profile_details_by_id(conn:dict, Id:str)->dict:
     """
     return get(conn, PCC_PROFILE + "/" + Id + "/details")
 
-## Provisions (empty)
+## Provisions
+def get_provisions(conn:dict)->dict:
+    """
+    Get Provisions
+
+    [Args]
+        (dict) conn: Connection dictionary obtained after logging in
+
+    [Returns]
+        (dict) Response: Get Provision response (includes any errors)
+    """
+    return get(conn, PCC_PROVISIONS)
+
+def add_provision(conn:dict, data:dict)->dict:
+    """
+    Add Provision
+
+    [Args]
+        (dict) conn: Connection dictionary obtained after logging in
+        (dict) data: provision
+                {
+                    "Clean": true,
+                    "EndTime": "2020-03-24T05:33:00.461Z",
+                    "ID": 0,
+                    "Rollback": true,
+                    "StartTime": "2020-03-24T05:33:00.461Z",
+                    "Status": "string",
+                    "execute": true,
+                    "statuses": [
+                        {
+                        "AppID": "string",
+                        "Configuration": {
+                            "AppID": "string",
+                            "Description": "string",
+                            "Files": [
+                            {
+                                "ConfigurationID": 0,
+                                "Content": "string",
+                                "ID": 0,
+                                "Name": "string"
+                            }
+                            ],
+                            "ID": 0,
+                            "Name": "string",
+                            "Versions": [
+                            "string"
+                            ]
+                        },
+                        "ConfigurationID": 0,
+                        "ID": 0,
+                        "Message": "string",
+                        "NodeID": 0,
+                        "Progress": 0,
+                        "ProvisionID": 0,
+                        "Result": "string",
+                        "level": "string",
+                        "metaData": "string",
+                        "operation": "string",
+                        "timeout": 0,
+                        "version": "string"
+                        }
+                    ],
+                    "template": {
+                        "id": 0,
+                        "ids": [
+                        0
+                        ],
+                        "nodes": [
+                        0
+                        ]
+                    },
+                    "type": "string"
+                }
+    [Returns]
+        (dict) Response: Add Provision response (includes any errors)
+    """
+    return post(conn, PCC_PROVISIONS, data)
+
+def reapply_provision_for_node(conn:dict, nodeId:str)->dict:
+    """
+    Reapply Provision for a Node
+
+    [Args]
+        (dict) conn: Connection dictionary obtained after logging in
+        (str) nodeId: nodeId
+
+    [Returns]
+        (dict) Response: Get Provision response (includes any errors)
+    """
+    return get(conn, PCC_PROVISIONS + "/reapply/" + nodeId)
+
+def get_provision_by_id(conn:dict, Id:str)->dict:
+    """
+    Get Provision by Id
+
+    [Args]
+        (dict) conn: Connection dictionary obtained after logging in
+        (str) Id: Id
+
+    [Returns]
+        (dict) Response: Get Provision response (includes any errors)
+    """
+    return get(conn, PCC_PROVISIONS + "/" + Id)
+
+def modify_provision_by_id(conn:dict, Id:str, data:dict)->dict:
+    """
+    Modify Provision by Id
+
+    [Args]
+        (dict) conn: Connection dictionary obtained after logging in
+        (str) Id: Id
+
+        (dict) data: provision
+                {
+                    "Clean": true,
+                    "EndTime": "2020-03-24T05:42:03.322Z",
+                    "ID": 0,
+                    "Rollback": true,
+                    "StartTime": "2020-03-24T05:42:03.322Z",
+                    "Status": "string",
+                    "execute": true,
+                    "statuses": [
+                        {
+                        "AppID": "string",
+                        "Configuration": {
+                            "AppID": "string",
+                            "Description": "string",
+                            "Files": [
+                            {
+                                "ConfigurationID": 0,
+                                "Content": "string",
+                                "ID": 0,
+                                "Name": "string"
+                            }
+                            ],
+                            "ID": 0,
+                            "Name": "string",
+                            "Versions": [
+                            "string"
+                            ]
+                        },
+                        "ConfigurationID": 0,
+                        "ID": 0,
+                        "Message": "string",
+                        "NodeID": 0,
+                        "Progress": 0,
+                        "ProvisionID": 0,
+                        "Result": "string",
+                        "level": "string",
+                        "metaData": "string",
+                        "operation": "string",
+                        "timeout": 0,
+                        "version": "string"
+                        }
+                    ],
+                    "template": {
+                        "id": 0,
+                        "ids": [
+                        0
+                        ],
+                        "nodes": [
+                        0
+                        ]
+                    },
+                    "type": "string"
+                }
+
+    [Returns]
+        (dict) Response: Modify Provision response (includes any errors)
+    """
+    return put(conn, PCC_PROVISIONS + "/" + Id, data)
+
+def add_provision_by_id(conn:dict, Id:str, data:dict):
+    """
+    Provision launches a provisioning
+
+    [Args]        
+        (dict) conn: Connection dictionary obtained after logging in
+        (str) Id: Id
+
+        (dict) data: provision
+                {
+                    "Clean": true,
+                    "EndTime": "2020-03-24T05:45:36.315Z",
+                    "ID": 0,
+                    "Rollback": true,
+                    "StartTime": "2020-03-24T05:45:36.315Z",
+                    "Status": "string",
+                    "execute": true,
+                    "statuses": [
+                        {
+                        "AppID": "string",
+                        "Configuration": {
+                            "AppID": "string",
+                            "Description": "string",
+                            "Files": [
+                            {
+                                "ConfigurationID": 0,
+                                "Content": "string",
+                                "ID": 0,
+                                "Name": "string"
+                            }
+                            ],
+                            "ID": 0,
+                            "Name": "string",
+                            "Versions": [
+                            "string"
+                            ]
+                        },
+                        "ConfigurationID": 0,
+                        "ID": 0,
+                        "Message": "string",
+                        "NodeID": 0,
+                        "Progress": 0,
+                        "ProvisionID": 0,
+                        "Result": "string",
+                        "level": "string",
+                        "metaData": "string",
+                        "operation": "string",
+                        "timeout": 0,
+                        "version": "string"
+                        }
+                    ],
+                    "template": {
+                        "id": 0,
+                        "ids": [
+                        0
+                        ],
+                        "nodes": [
+                        0
+                        ]
+                    },
+                    "type": "string"
+                }
+
+    [Returns]
+        (dict) Response: Add Provision response (includes any errors)
+    """
+    return post(conn, PCC_PROVISIONS + "/" + Id, data)
+
+
+def get_provision_status_by_id(conn:dict, Id:str)->dict:
+    """
+    Get Provision Status by Id
+
+    [Args]
+        (dict) conn: Connection dictionary obtained after logging in
+        (str) Id: Id
+
+    [Returns]
+        (dict) Response: Get Provision response (includes any errors)
+    """
+    return get(conn, PCC_PROVISIONS + "/" + Id + "/statuses")
+
 
 
 ## Roles
