e81899598772ab61b224f1490b06178171a48eb4
Gianluca Arena <arena.gianluca@outlook.it>
2023-06-29 12:04:07 +0200

    Add User endpoints

--- python/platina_sdk/s3/s3_api.py
+++ python/platina_sdk/s3/s3_api.py
@@ -10,7 +10,7 @@ from platina_sdk.utils import get, post, put, delete, post_multipart, put_multip
 S3MANAGER = "/s3-manager"
 S3PCCS = S3MANAGER + "/pccs"
 S3ORGS = S3MANAGER + "/customers"
-
+S3USERS = "/user-management/user"
 ### PCC instances ###
 
 def get_pccs(conn: dict) -> dict:
@@ -223,6 +223,115 @@ def delete_organization(conn: dict, id: str) -> dict:
         (dict) Response: Result of the operation
     """
     return delete(conn, S3ORGS + "/" + id)
+
+### S3 Users ###
+
+def get_users(conn: dict) -> dict:
+    """
+    Get Users
+
+    [Args]
+        (dict) conn: Connection dictionary obtained after logging in
+
+    [Returns]
+        (dict) Response["data"]: [{
+                "id": 3,
+                "username": "test-org@test.com",
+                "active": true,
+                "profile": {
+                  "id": 3,
+                  "firstname": "test-org",
+                  "lastname": "test-org",
+                  "email": "test-org@test.com"
+                },
+                "role": {...},
+                "tenant": {...},
+                "protect": false,
+                "application": false,
+                "owner": 3
+              },
+            ]
+    """
+    return get(conn, S3USERS + "/list")
+
+def create_user(conn: dict,  data: dict) -> dict:
+    """
+    Add User
+
+    [Args]
+        (dict) conn: Connection dictionary obtained after logging in
+        (dict) data: {
+                  "username": "platina-1@test.com",
+                  "tenant": 1,
+                  "firstname": "platina-1",
+                  "lastname": "platina-1",
+                  "password": "platina-1",
+                  "email": "platina-1@test.com",
+                  "roleID": 1,
+                  "active": true,
+                  "source": "https://172.17.2.221:59999/s3-portal-admin/reset-password?",
+                  "protect": false
+                }
+
+    [Returns]
+        (dict) Response["data"]: {
+                              "id": 6,
+                              "username": "platina-1@test.com",
+                              "active": true,
+                              "profile": null,
+                              "role": {...},
+                              "tenant": {...},
+                              "protect": false,
+                              "application": false,
+                              "owner": 1
+                            }
+    """
+    return post(conn, S3USERS + "/register", data)
+
+def update_user(conn: dict, data: dict) -> dict:
+    """
+    Update User
+
+    [Args]
+        (dict) conn: Connection dictionary obtained after logging in
+        (dict)    {
+              "id": 6,
+              "username": "platina-1@test.com",
+              "firstname": "platina-1-modif",
+              "lastname": "platina-1",
+              "email": "platina-1@test.com",
+              "active": true,
+              "protect": false
+            }
+
+    [Returns]
+        (dict) Response["data"]:  {
+                              "id": 6,
+                              "username": "platina-1@test.com",
+                              "active": true,
+                              "profile": null,
+                              "role": {...},
+                              "tenant": {...},
+                              "protect": false,
+                              "application": false,
+                              "owner": 1
+                            }
+    """
+    return put(conn, S3USERS + "/update", data)
+
+def delete_user(conn: dict, data: dict) -> dict:
+    """
+    Delete User
+
+    [Args]
+        (dict) conn: Connection dictionary obtained after logging in
+        (dict) {“username”:“platina-1@test.com”}
+
+    [Returns]
+        (dict) Response: Result of the operation
+    """
+    return delete(conn, S3USERS + "/delete", data)
+
 
 
 
