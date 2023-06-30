1de853dcd2386a11ed7814b8f5c3dd9187158457
Gianluca Arena <arena.gianluca@outlook.it>
2023-06-30 12:03:25 +0200

    Fix pcc utility endpoints

--- python/platina_sdk/s3/s3_api.py
+++ python/platina_sdk/s3/s3_api.py
@@ -255,7 +255,7 @@ def get_rgwnodes_by_cluster(conn: dict, pccID: str, clusterID: str) -> dict:
                   ]
 
     """
-    return get(conn, S3PCCS + pccID + "/storage/clusters/" + clusterID + "/nodes/cephrgw")
+    return get(conn, S3PCCS + "/" + pccID + "/storage/clusters/" + clusterID + "/nodes/cephrgw")
 
 ### Organizations ###
 
